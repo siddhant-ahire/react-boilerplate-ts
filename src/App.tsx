4c1033d54ebe1dad6f95d5a658553ff4dbaff1bc
siddhant ahire <siddhantahire@siddhants-Mac-mini.local>
2024-08-25 19:23:30 +0530

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(): React.JSX.Element {
+  useEffect(() => {
+    console.log('hello');
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
